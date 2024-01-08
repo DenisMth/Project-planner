550cb5786ed02069e0cea508f225b32c3fef9e07
Denis Mathieu <denismathieu1203@gmail.com>
2024-01-08 17:03:30 +0100

    added timer remaining

---
+++ assets/script/remainingTime.js
@@ -0,0 +1,39 @@
+import * as dateFns from 'https://cdn.jsdelivr.net/npm/date-fns@2.24.0/esm/index.js';
+
+function displayExisting(variable, name, elem){
+    if (variable){
+        let text = document.createTextNode(variable + " " + name + " ");
+        elem.appendChild(text);
+    }
+}
+
+function deleteTitle(){
+    let title = document.querySelector("h1");
+    if (title){
+        title.remove();
+    }
+}
+
+function newTime(){
+
+    deleteTitle();
+
+    let now = new Date();
+    let future = new Date('2024-01-23');
+    let interval = dateFns.intervalToDuration({start : now, end : future});
+    let body = document.querySelector("body");
+    let title = document.createElement("h1");
+    let titleContent = document.createTextNode("");
+
+    for (let element in interval) {
+        displayExisting(interval[element], element, title);
+    };
+    let text = document.createTextNode("remaining");
+    title.appendChild(text);
+
+    title.appendChild(titleContent);
+    body.appendChild(title);
+    
+}
+
+setInterval(newTime, 1000);
